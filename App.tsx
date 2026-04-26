7bb9f5f20c5eac001cb1776831b3426a548c9b64
Ariel Sousa Dias <114015507+perigoboy@users.noreply.github.com>
2026-04-26 15:17:53 -0300

    docs: finaliza estrutura de navegação da task #4

--- App.tsx
+++ App.tsx
@@ -1,53 +1,6 @@
-import React, { useEffect } from 'react';
-import { SafeAreaView, Text, Button, Alert, View, StyleSheet } from 'react-native';
-import { authService } from './src/service/authService';
+import React from 'react';
+import AppNavigator from './src/navigation/AppNavigator';
 
-const App = () => {
-  const handleTesteCadastro = async () => {
-    try {
-      await authService.signUp("teste@cordel.com", "123456");
-      Alert.alert("Sucesso!", "Usuário criado no Firebase!");
-    } catch (error: any) {
-      Alert.alert("Erro", error.message);
-    }
-  };
-
-  useEffect(() => {
-    const unsubscribe = authService.onAuthStateChange((user: any) => {
-      if (user) {
-        console.log("Usuário logado:", user.email);
-      } else {
-        console.log("Nenhum usuário logado.");
-      }
-    });
-    return unsubscribe;
-  }, []);
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.title}>Projeto Cordel - Teste Task #3</Text>
-        <Button title="Testar Cadastro Firebase" onPress={handleTesteCadastro} />
-      </View>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 20,
-  },
-});
-
-export default App;
-  
+export default function App() {
+  return <AppNavigator />;
+}
---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,13 @@
+//Isso é um exemplo de tela de Login. Você pode personalizá-la como quiser.
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { authService } from '../service/authService';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Bem-vindo à Home!</Text>
+      <Button title="Sair" onPress={() => authService.logout()} />
+    </View>
+  );
+}
